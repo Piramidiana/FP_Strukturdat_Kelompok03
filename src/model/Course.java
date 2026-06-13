484833e3fa94100959e7f447fed9ea8877959460
RAVEN-031 <izzatwahyudi07@gmail.com>
2026-06-14 00:44:04 +0700

    Feat Course object

--- src/model/Course.java
+++ src/model/Course.java
@@ -7,12 +7,16 @@ public class Course {
     private String name;
     private int sks;
     private int semester;
+    private String major;
+    private String desc;
 
-    public Course(String code, String name, int sks, int semester) {
+    public Course(String code, String name, int sks, int semester, String major, String desc) {
         this.code = code;
         this.name = name;
         this.sks = sks;
         this.semester = semester;
+        this.major = major;
+        this.desc = desc;
     }
 
     public String getCode() {
@@ -31,6 +35,14 @@ public class Course {
         return semester;
     }
 
+    public String getMajor() {
+        return major;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -43,6 +55,14 @@ public class Course {
         this.semester = semester;
     }
 
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+    
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
